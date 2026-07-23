a41e294951aa296f6c023dbe1c79fc626e018140
n01028447 <n01028447@humber.ca>
2026-07-23 18:10:17 -0400

    Add files via upload

---
+++ part2.js
@@ -0,0 +1,19 @@
+let studentInfo = {
+    name: "John Cena",
+    age: 122,
+    enrollment: true,
+    courses: [" Biology", " Chemistry", " English", " Calculus", " Psychology"],
+    display: function () {
+        console.log("Hello my name is " + this.name);
+        console.log("I am " + this.age + " years old");
+        console.log("I'm currently taking:" + this.courses);
+    }
+};
+
+let infoString = JSON.stringify(studentInfo);
+console.log(infoString);
+
+let infoObj = JSON.parse(infoString);
+console.log(infoObj); //The function is omited for the newly created object
+
+console.log(studentInfo);
